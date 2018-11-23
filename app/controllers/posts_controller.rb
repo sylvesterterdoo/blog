b87c7ff06c1c867cdee9acbb294951c70320c68d
Sylvester Terdoo <sylvesterterdoo@gmail.com>
2018-11-23 16:23:22 +0200

    before textbook

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,32 @@
+class PostsController < ApplicationController
+
+  def index
+    @great_grand_father = 'Jinpachi'    
+    @grand_father = 'Heihachi'    
+    @father = 'Kazuya'    
+    @fighter = 'Jin'    
+  end
+  
+  def new
+
+  end
+
+  def edit 
+    
+  end
+
+  def create 
+    
+  end
+
+  def show
+
+  end
+
+
+
+
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   get 'welcome/index'
-  resources :articles
+  get 'posts/index'
+  get 'posts/new'
+  get 'posts/edit'
+  get 'posts/create'
+  get 'posts/show'
+  resource :articles
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
